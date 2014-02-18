8bd2310379a7f19cd9e03d604928555e3ca3f1f7
Chroma Developer <leif@structuresound.com>
2014-02-18 13:28:57 -0800

    depth buffer fix

--- example-ofxTableView/src/main.mm
+++ example-ofxTableView/src/main.mm
@@ -5,6 +5,7 @@ int main(){
 	ofSetupOpenGL(1024,768,OF_FULLSCREEN);			// <-------- setup the GL context
 
     ofxiOSGetOFWindow()->enableRetina();
+    ofxiOSGetOFWindow()->enableDepthBuffer();
     
 	ofRunApp(new testApp());
 }
